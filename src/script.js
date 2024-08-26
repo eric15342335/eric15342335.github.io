675fe9943b32ad51099f3872092a97161e389277
Cheng Ho Ming, Eric <eric310@connect.hku.hk>
2024-08-26 11:03:01 +0800

    Cleanup javascript

--- src/script.js
+++ src/script.js
@@ -1,40 +1,33 @@
 'use strict';
 
-// Constants for magic variables
 const RICKROLL_URL = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
 const CONFIRM_MESSAGE = 'Are you sure you want to leave this page?';
-const VISIBLE_TITLE = "Eric's GitHub Pages";
-const HIDDEN_TITLE = "Come back to " + VISIBLE_TITLE + "!";
+const ORIGINAL_TITLE = document.title;
+const COMEBACK_TITLE = `Come back to ${ORIGINAL_TITLE}!`;
 const RICKROLL_BUTTON_ID = 'rickroll-button';
 
-/* Modifying button requires the DOM to be fully loaded */
+// Modifying button requires the DOM to be fully loaded
 document.addEventListener('DOMContentLoaded', () => {
   const rickrollButton = document.getElementById(RICKROLL_BUTTON_ID);
   if (rickrollButton) {
     rickrollButton.addEventListener('click', redirectToYouTube);
   }
 
-  // Add event listener for visibility change
   document.addEventListener('visibilitychange', handleVisibilityChange);
 });
 
-// Function to redirect to YouTube
 const redirectToYouTube = () => {
   if (confirm(CONFIRM_MESSAGE)) {
     window.location.href = RICKROLL_URL;
   } else {
-    /* Removes the button */
     document.getElementById(RICKROLL_BUTTON_ID).remove();
   }
 };
 
-// Function to handle visibility change
 const handleVisibilityChange = () => {
   if (document.visibilityState === 'hidden') {
-    // Change the title when the tab is not focused
-    document.title = HIDDEN_TITLE;
+    document.title = COMEBACK_TITLE;
   } else {
-    // Restore the original title when the tab is focused
-    document.title = VISIBLE_TITLE;
+    document.title = ORIGINAL_TITLE;
   }
 };
