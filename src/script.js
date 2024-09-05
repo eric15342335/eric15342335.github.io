718b57cdcff863f504b20dba52726b735ce9ba13
Cheng Ho Ming, Eric <eric310@connect.hku.hk>
2024-09-05 22:19:08 +0800

    feat: Update 404 page design and functionality
    - Create a new 404.html file with an improved design and user-friendly messaging
    - Add a countdown timer to automatically redirect users to the home page after 10 seconds
    - Implement fade-in and fade-out animation for the countdown timer
    - Remove unnecessary files: assets/index.html, pages/common/index.html, pages/index.html, src/index.html
    
    run prettier on js files

--- src/script.js
+++ src/script.js
@@ -1,59 +1,62 @@
-'use strict';
+"use strict";
 
-const RICKROLL_URL = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
-const CONFIRM_MESSAGE = 'Are you sure you want to leave this page?';
-const RICKROLL_BUTTON_ID = 'rickroll-button';
-const BUTTON_REMOVED_KEY = 'rickrollButtonRemoved';
+const RICKROLL_URL = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+const CONFIRM_MESSAGE = "Are you sure you want to leave this page?";
+const RICKROLL_BUTTON_ID = "rickroll-button";
+const BUTTON_REMOVED_KEY = "rickrollButtonRemoved";
 
 const ORIGINAL_TITLE = document.title;
 const COMEBACK_TITLE = `Come back to ${ORIGINAL_TITLE}!`;
 
-const NAVBAR_HTML_PATH = '/pages/common/navbar.html';
-const NAVBAR_PLACEHOLDER_ID = 'nav-placeholder';
+const NAVBAR_HTML_PATH = "/pages/common/navbar.html";
+const NAVBAR_PLACEHOLDER_ID = "nav-placeholder";
 
-const FOOTER_HTML_PATH = '/pages/common/footer.html';
-const FOOTER_PLACEHOLDER_ID = 'footer-placeholder';
+const FOOTER_HTML_PATH = "/pages/common/footer.html";
+const FOOTER_PLACEHOLDER_ID = "footer-placeholder";
 
 const resizeDebounceTime = 100;
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.debug('DOMContentLoaded event fired');
-  document.addEventListener('visibilitychange', handleVisibilityChange);
+document.addEventListener("DOMContentLoaded", () => {
+  console.debug("DOMContentLoaded event fired");
+  document.addEventListener("visibilitychange", handleVisibilityChange);
 
   fetch(NAVBAR_HTML_PATH)
-    .then(response => response.text())
-    .then(data => {
+    .then((response) => response.text())
+    .then((data) => {
       document.getElementById(NAVBAR_PLACEHOLDER_ID).innerHTML = data;
-      
+
       const rickrollButton = document.getElementById(RICKROLL_BUTTON_ID);
       // Check if the button was previously removed
       if (buttonClickedBefore()) {
         removeRickrollButton();
       } else if (rickrollButton) {
-        rickrollButton.addEventListener('click', redirectToYouTube);
+        rickrollButton.addEventListener("click", redirectToYouTube);
       } else {
         console.warn(`Button with ID "${RICKROLL_BUTTON_ID}" not found.`);
       }
-      
+
       /* JavaScript to update the scroll indicator */
-      window.addEventListener('scroll', () => {
-        const scrollIndicator = document.querySelector('.scroll-indicator');
-        const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
+      window.addEventListener("scroll", () => {
+        const scrollIndicator = document.querySelector(".scroll-indicator");
+        const maxScroll =
+          document.documentElement.scrollHeight - window.innerHeight;
         const scrollPercentage = (window.scrollY / maxScroll) * 100;
-        scrollIndicator.style.backgroundPosition = `${100 - scrollPercentage}% 0`;
-        console.debug('Window scrolled');
+        scrollIndicator.style.backgroundPosition = `${
+          100 - scrollPercentage
+        }% 0`;
+        console.debug("Window scrolled");
       });
 
-      window.addEventListener('resize', handleWindowResize);
+      window.addEventListener("resize", handleWindowResize);
     })
-    .catch(error => console.error('Error loading navigation:', error));
-  
+    .catch((error) => console.error("Error loading navigation:", error));
+
   fetch(FOOTER_HTML_PATH)
-    .then(response => response.text())
-    .then(data => {
+    .then((response) => response.text())
+    .then((data) => {
       document.getElementById(FOOTER_PLACEHOLDER_ID).innerHTML = data;
     })
-    .catch(error => console.error('Error loading footer:', error));
+    .catch((error) => console.error("Error loading footer:", error));
 });
 
 /**
@@ -66,26 +69,28 @@ const redirectToYouTube = () => {
   } else {
     removeRickrollButton();
     // Cache the button removed state
-    sessionStorage.setItem(BUTTON_REMOVED_KEY, 'true');
+    sessionStorage.setItem(BUTTON_REMOVED_KEY, "true");
   }
 };
 
 const buttonClickedBefore = () => {
-  return sessionStorage.getItem(BUTTON_REMOVED_KEY) === 'true';
+  return sessionStorage.getItem(BUTTON_REMOVED_KEY) === "true";
 };
 
 const resetButtonState = () => {
   const rickrollButton = document.getElementById(RICKROLL_BUTTON_ID);
   sessionStorage.removeItem(BUTTON_REMOVED_KEY);
-  rickrollButton.style.display = 'block';
+  rickrollButton.style.display = "block";
 };
 
 const removeRickrollButton = () => {
   const rickrollButton = document.getElementById(RICKROLL_BUTTON_ID);
   if (rickrollButton) {
-    rickrollButton.style.display = 'none';
+    rickrollButton.style.display = "none";
   } else {
-    console.warn(`Button with ID "${RICKROLL_BUTTON_ID}" not found for removal.`);
+    console.warn(
+      `Button with ID "${RICKROLL_BUTTON_ID}" not found for removal.`
+    );
   }
 };
 
@@ -95,7 +100,7 @@ const removeRickrollButton = () => {
  * If the document becomes visible again, sets the document title to ORIGINAL_TITLE.
  */
 const handleVisibilityChange = () => {
-  if (document.visibilityState === 'hidden') {
+  if (document.visibilityState === "hidden") {
     document.title = COMEBACK_TITLE;
   } else {
     document.title = ORIGINAL_TITLE;
@@ -120,7 +125,6 @@ const handleWindowResize = debounce(() => {
     } else {
       console.warn(`Button with ID "${RICKROLL_BUTTON_ID}" not found.`);
     }
-    console.debug('Window resized');
+    console.debug("Window resized");
   }
 }, resizeDebounceTime);
-
--- src/sitemap.js
+++ src/sitemap.js
@@ -1,26 +1,26 @@
-'use strict';
+"use strict";
 
 /**
  * Fetches the sitemap.xml file, parses the URLs, and displays them in a list on the page.
  * This function is executed when the DOM content has finished loading.
  */
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('/sitemap.xml')
-    .then(response => response.text())
-    .then(data => {
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("/sitemap.xml")
+    .then((response) => response.text())
+    .then((data) => {
       const parser = new DOMParser();
-      const xmlDoc = parser.parseFromString(data, 'application/xml');
-      const urls = xmlDoc.querySelectorAll('url');
-      const sitemapList = document.getElementById('sitemap-menu');
+      const xmlDoc = parser.parseFromString(data, "application/xml");
+      const urls = xmlDoc.querySelectorAll("url");
+      const sitemapList = document.getElementById("sitemap-menu");
 
-      urls.forEach(url => {
-        const listItem = document.createElement('li');
-        const link = document.createElement('a');
+      urls.forEach((url) => {
+        const listItem = document.createElement("li");
+        const link = document.createElement("a");
         link.href = url.textContent;
         link.textContent = url.textContent;
         listItem.appendChild(link);
         sitemapList.appendChild(listItem);
       });
     })
-    .catch(error => console.error('Error fetching the sitemap:', error));
+    .catch((error) => console.error("Error fetching the sitemap:", error));
 });
